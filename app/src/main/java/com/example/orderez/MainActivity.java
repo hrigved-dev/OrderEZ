f0e4b18471e663caccb244fcda319d9a2bb9a0b5
hrigved-dev <hrigved.khatavkar@gmail.com>
2021-05-18 14:43:30 +0530

    Completed Splash Screen

--- app/src/main/java/com/example/orderez/MainActivity.java
+++ app/src/main/java/com/example/orderez/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.orderez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    Thread timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timer = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    synchronized (this) {
+                        wait(3000);
+                    }
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(MainActivity.this, AuthScreen.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 }
