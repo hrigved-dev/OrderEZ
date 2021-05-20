df6df315cd197968bbcee02dd01fb1ffdc57ec15
hrigved-dev <hrigved.khatavkar@gmail.com>
2021-05-20 19:47:04 +0530

    Completed the XML design for the Login Screen

--- app/src/main/java/com/example/orderez/AuthScreen.java
+++ app/src/main/java/com/example/orderez/AuthScreen.java
@@ -1,14 +1,52 @@
 package com.example.orderez;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AuthScreen extends AppCompatActivity {
+
+    TextInputLayout t1, t2;
+    ProgressBar bar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth_screen);
+        t1 = (TextInputLayout)findViewById(R.id.email);
+        t2 = (TextInputLayout)findViewById(R.id.password);
+        bar = (ProgressBar)findViewById(R.id.progressBar);
+    }
+
+    public void signUp(View view) {
+        String email = t1.getEditText().getText().toString();
+        String password = t2.getEditText().getText().toString();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(AuthScreen.this,  new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+
+                } else {
+
+                }
+            }
+        });
     }
 }
