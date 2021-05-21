58dfcb13ddebd4e939cd26c4cd33dc47ec01c1aa
hrigved-dev <hrigved.khatavkar@gmail.com>
2021-05-21 20:07:37 +0530

    Logout functionality added

--- app/src/main/java/com/example/orderez/Dashboard.java
+++ app/src/main/java/com/example/orderez/Dashboard.java
@@ -2,13 +2,32 @@ package com.example.orderez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Dashboard extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+    }
+
+    public void signOut(View view) {
+        mAuth.signOut();
+        Toast.makeText(getApplicationContext(), "Logged Out Successfully", Toast.LENGTH_LONG).show();
+        Intent myIntent = new Intent(Dashboard.this, AuthScreen.class);
+        startActivity(myIntent);
+        finish();
     }
 }
