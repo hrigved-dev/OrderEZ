7bc99d75764b926629cb6f2ca605d51d93f9b3ce
hrigved-dev <hrigved.khatavkar@gmail.com>
2021-05-22 15:42:59 +0530

    Added Bottom Modal

--- app/src/main/java/com/example/orderez/myadapter.java
+++ app/src/main/java/com/example/orderez/myadapter.java
@@ -3,8 +3,11 @@ package com.example.orderez;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewAnimator;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,6 +15,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -22,11 +27,36 @@ public class myadapter extends FirebaseRecyclerAdapter<model, myadapter.myViewHo
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull myadapter.myViewHolder holder, int position, @NonNull model model) {
+    protected void onBindViewHolder(@NonNull myadapter.myViewHolder holder, final int position, @NonNull model model) {
         holder.nameText.setText(model.getName());
         holder.orderText.setText(model.getOrder());
         holder.extraText.setText(model.getExtra());
 //        Glide.with(holder.img.getContext()).load(model.getPurl()).into(holder.img)
+        holder.edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final DialogPlus dialogPlus = DialogPlus.newDialog(holder.img.getContext()).setContentHolder(new ViewHolder(R.layout.dialogcontent)).setExpanded(true, 1450).create();
+
+                View myView = dialogPlus.getHolderView();
+                EditText name = myView.findViewById(R.id.nameText);
+                EditText order = myView.findViewById(R.id.orderText);
+                EditText extra = myView.findViewById(R.id.extraText);
+                Button update = myView.findViewById(R.id.update);
+
+                name.setText(model.getName());
+                order.setText(model.getOrder());
+                extra.setText(model.getExtra());
+
+                dialogPlus.show();
+
+                update.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+            }
+        });
     }
 
     @NonNull
@@ -40,6 +70,7 @@ public class myadapter extends FirebaseRecyclerAdapter<model, myadapter.myViewHo
 
         CircleImageView img;
         TextView nameText, orderText, extraText;
+        ImageView edit, delete;
 
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -47,6 +78,8 @@ public class myadapter extends FirebaseRecyclerAdapter<model, myadapter.myViewHo
             nameText = (TextView)itemView.findViewById(R.id.nameText);
             orderText = (TextView)itemView.findViewById(R.id.orderText);
             extraText = (TextView)itemView.findViewById(R.id.extraText);
+            edit = (ImageView)itemView.findViewById(R.id.editIcon);
+            delete = (ImageView)itemView.findViewById(R.id.deleteIcon);
         }
     }
 }
