219de1cb568c00c4414825eea023506e14fb9377
hrigved-dev <hrigved.khatavkar@gmail.com>
2021-06-13 16:22:49 +0530

    Completed Add Data Screen with Logic

--- app/src/main/java/com/example/orderez/AddData.java
+++ app/src/main/java/com/example/orderez/AddData.java
@@ -1,5 +1,6 @@
 package com.example.orderez;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,18 +10,39 @@ import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddData extends AppCompatActivity {
 
     ImageView back;
     TextView logo;
+    TextInputLayout addName, addOrder, addExtra;
+    Button addData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_data);
+
+        addName = (TextInputLayout) findViewById(R.id.nameText);
+        addOrder = (TextInputLayout) findViewById(R.id.orderText);
+        addExtra = (TextInputLayout) findViewById(R.id.extraText);
+
+        addData = (Button) findViewById(R.id.addData);
 
         back = (ImageView)findViewById(R.id.back);
 
@@ -35,6 +57,36 @@ public class AddData extends AppCompatActivity {
                 Intent myIntent = new Intent(AddData.this, Dashboard.class);
                 startActivity(myIntent);
                 finish();
+            }
+        });
+
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                processInsert();
+            }
+        });
+    }
+
+    private void processInsert() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", addName.getEditText().getText().toString());
+        map.put("order", addOrder.getEditText().getText().toString());
+        map.put("extra", addExtra.getEditText().getText().toString());
+
+        FirebaseDatabase.getInstance().getReference().child("users").push().setValue(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Toast.makeText(getApplicationContext(), "Added your Order Successfully", Toast.LENGTH_LONG).show();
+                Intent myIntent = new Intent(AddData.this, Dashboard.class);
+                startActivity(myIntent);
+                finish();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Sorry,your Order was not inserted", Toast.LENGTH_LONG).show();
             }
         });
     }
